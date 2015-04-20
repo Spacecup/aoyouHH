f2aa5636c3e207837baa675bcfbb116e2f474c5f
ljz <863784757@qq.com>
2015-04-20 15:02:36 +0800

    新建工程

---
+++ aoyouHH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aoyouHH
+//
+//  Created by jinzelu on 15/4/20.
+//  Copyright (c) 2015年 jinzelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
