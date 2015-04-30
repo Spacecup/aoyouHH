a34ebde7397aa2e1734f7864d15b6fce7f0d00e5
ljz <863784757@qq.com>
2015-04-30 16:48:30 +0800

    优化图片浏览功能，新增单击退出，双击放大，缩小，两个手指缩放功能。

--- aoyouHH/Views/JZAlbumViewController.m
+++ aoyouHH/Views/JZAlbumViewController.m
@@ -9,11 +9,13 @@
 #import "JZAlbumViewController.h"
 #import "UIImageView+WebCache.h"
 #import "MBProgressHUD.h"
+#import "PhotoView.h"
 
-@interface JZAlbumViewController ()<UIScrollViewDelegate>
+@interface JZAlbumViewController ()<UIScrollViewDelegate,PhotoViewDelegate>
 {
     CGFloat lastScale;
     MBProgressHUD *HUD;
+    NSMutableArray *_subViewList;
 }
 
 @end
@@ -24,7 +26,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         //
-        self.edgesForExtendedLayout = UIRectEdgeAll;
+        _subViewList = [[NSMutableArray alloc] init];
     }
     return self;
 }
@@ -35,9 +37,9 @@
     lastScale = 1.0;
     self.view.backgroundColor = [UIColor blackColor];
     
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(OnTapView)];
-    [self.view addGestureRecognizer:tap];
-    
+//    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(OnTapView)];
+//    [self.view addGestureRecognizer:tap];
+
     [self initScrollView];
     [self addLabels];
     [self setPicCurrentIndex:self.currentIndex];
@@ -49,17 +51,24 @@
 }
 
 -(void)initScrollView{
-    [[SDImageCache sharedImageCache] cleanDisk];
-    [[SDImageCache sharedImageCache] clearMemory];
+//    [[SDImageCache sharedImageCache] cleanDisk];
+//    [[SDImageCache sharedImageCache] clearMemory];
     self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, screen_width, screen_height)];
     self.scrollView.pagingEnabled = YES;
     self.scrollView.userInteractionEnabled = YES;
     self.scrollView.showsHorizontalScrollIndicator = NO;
+    
     self.scrollView.contentSize = CGSizeMake(self.imgArr.count*screen_width, screen_height);
     self.scrollView.delegate = self;
     self.scrollView.contentOffset = CGPointMake(0, 0);
+    //设置放大缩小的最大，最小倍数
+//    self.scrollView.minimumZoomScale = 1;
+//    self.scrollView.maximumZoomScale = 2;
     [self.view addSubview:self.scrollView];
     
+    for (int i = 0; i < self.imgArr.count; i++) {
+        [_subViewList addObject:[NSNull class]];
+    }
 
 }
 
@@ -67,79 +76,54 @@
     self.sliderLabel = [[UILabel alloc] initWithFrame:CGRectMake(40, screen_height-64-49, 60, 30)];
     self.sliderLabel.backgroundColor = [UIColor clearColor];
     self.sliderLabel.textColor = [UIColor whiteColor];
-    self.sliderLabel.text = [NSString stringWithFormat:@"%ld/%ld",self.currentIndex+1,self.imgArr.count];
+    self.sliderLabel.text = [NSString stringWithFormat:@"%ld/%lu",self.currentIndex+1,(unsigned long)self.imgArr.count];
     [self.view addSubview:self.sliderLabel];
 }
 
 -(void)setPicCurrentIndex:(NSInteger)currentIndex{
     _currentIndex = currentIndex;
-    self.scrollView.contentOffset = CGPointMake(screen_width*currentIndex, 0);
+//    self.scrollView.contentOffset = CGPointMake(screen_width*currentIndex, 0);
     [self loadPhote:_currentIndex];
+    [self loadPhote:_currentIndex+1];
+    [self loadPhote:_currentIndex-1];
 }
 
 -(void)loadPhote:(NSInteger)index{
     if (index<0 || index >=self.imgArr.count) {
         return;
     }
-    float width = self.view.frame.size.width;
-    float height = self.view.frame.size.height;
     
-    id path = [self.imgArr objectAtIndex:index];
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(width*index, 0, width, height)];
-    imageView.tag = self.currentIndex;
-    imageView.contentMode = UIViewContentModeScaleAspectFit;
-    imageView.userInteractionEnabled = YES;
-    //放大手势
-    UIPinchGestureRecognizer *pinGes = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinGes:)];
-    
-    [imageView addGestureRecognizer:pinGes];
-    
-    
-    [self.scrollView addSubview:imageView];
-    
-    if ([path isKindOfClass:[NSString class]]) {
-        SDWebImageManager *manager = [SDWebImageManager sharedManager];
-        BOOL isCached = [manager cachedImageExistsForURL:[NSURL URLWithString:path]];
-        if(!isCached){//没有缓存
-            NSLog(@"1111没有缓存");
-            HUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-            HUD.mode = MBProgressHUDModeDeterminate;
-        }
-        
-//            [imageView sd_setImageWithURL:[NSURL URLWithString:path] placeholderImage:[UIImage imageNamed:@"comment_empty_img"]];
-//         第三方UIActivityIndicator库，暂时没用到
-//            [imageView setImageWithURL:[NSURL URLWithString:path] placeholderImage:[UIImage imageNamed:@"comment_empty_img"] usingActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-        [imageView sd_setImageWithURL:[NSURL URLWithString:path] placeholderImage:[UIImage imageNamed:@"comment_empty_img"] options:0 progress:^(NSInteger receivedSize, NSInteger expectedSize){
-            HUD.progress = ((float)receivedSize)/expectedSize;
-        } completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL){
-            NSLog(@"加载完成");
-            if (!isCached) {
-                [HUD hide:YES];
-            }
-            
-        }];
-        NSLog(@"url:%@",path);
-    }else if([path isKindOfClass:[UIImage class]]){
-        [imageView setImage:[self.imgArr objectAtIndex:index]];
+    id currentPhotoView = [_subViewList objectAtIndex:index];
+    if (![currentPhotoView isKindOfClass:[PhotoView class]]) {
+        //url数组
+        CGRect frame = CGRectMake(index*_scrollView.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height);
+        PhotoView *photoV = [[PhotoView alloc] initWithFrame:frame withPhotoUrl:[self.imgArr objectAtIndex:index]];
+        photoV.delegate = self;
+        [self.scrollView insertSubview:photoV atIndex:0];
+        [_subViewList replaceObjectAtIndex:index withObject:photoV];
     }else{
-        [imageView setImage:[UIImage imageNamed:@"comment_empty_img"]];
+        PhotoView *photoV = (PhotoView *)currentPhotoView;
     }
     
 }
 
-
--(void)OnTapView{
-//    [self.navigationController popViewControllerAnimated:YES];
+#pragma mark - PhotoViewDelegate
+-(void)TapHiddenPhotoView{
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+-(void)OnTapView{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+//手势
 -(void)pinGes:(UIPinchGestureRecognizer *)sender{
     if ([sender state] == UIGestureRecognizerStateBegan) {
         lastScale = 1.0;
     }
     CGFloat scale = 1.0 - (lastScale -[sender scale]);
     lastScale = [sender scale];
-    
+    self.scrollView.contentSize = CGSizeMake(self.imgArr.count*screen_width, screen_height*lastScale);
+    NSLog(@"scale:%f   lastScale:%f",scale,lastScale);
     CATransform3D newTransform = CATransform3DScale(sender.view.layer.transform, scale, scale, 1);
     
     sender.view.layer.transform = newTransform;
@@ -153,7 +137,7 @@
     NSLog(@"scrollViewDidEndDecelerating");
     int i = scrollView.contentOffset.x/screen_width+1;
     [self loadPhote:i-1];
-    self.sliderLabel.text = [NSString stringWithFormat:@"%d/%ld",i,self.imgArr.count];
+    self.sliderLabel.text = [NSString stringWithFormat:@"%d/%lu",i,(unsigned long)self.imgArr.count];
 }
 
 
